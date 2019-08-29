c6d20647fc3dc383001acc7fe17b67679e7e0ef6
Devany Noblit <47183931+DNoblit@users.noreply.github.com>
2019-08-28 21:44:15 -0400

    Add files via upload

---
+++ OrderDrinkController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BartenderApp.Models
+{
+    public class OrderDrink
+    {
+        public int OrderId { get; set; }
+        public string Patron { get; set; }
+        public string DrinkType { get; set; }
+    }
+}
